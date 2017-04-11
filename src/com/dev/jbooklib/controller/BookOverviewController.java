f350cd60e18130e5942c1949031e4497905e6edb
vetal71 <vetalkolesnik@gmail.com>
2017-04-11 18:04:36 +0300

    добавлен класс модели

--- src/com/dev/jbooklib/controller/BookOverviewController.java
+++ src/com/dev/jbooklib/controller/BookOverviewController.java
@@ -1,12 +1,34 @@
 package com.dev.jbooklib.controller;
 
 import com.dev.jbooklib.MainApp;
+import com.dev.jbooklib.model.Category;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TreeTableColumn;
+import javafx.scene.control.TreeTableView;
 
-public class BookOverviewController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class BookOverviewController implements Initializable {
 
     private MainApp mainApp;
 
+    @FXML
+    private TreeTableView<Category> tvCategory;
+    @FXML
+    private TreeTableColumn<Category, Integer> colId;
+    @FXML
+    private TreeTableColumn<Category, String> colName;
+    @FXML
+    private TreeTableColumn<Category, Integer> colCount;
+
     public void setMainApp(MainApp mainApp) {
         this.mainApp = mainApp;
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        //TODO
     }
 }
